4777b8d4f25fe9b745dcf5aec5ec6e17436d65ae
Jazmin Nugra <jazminn7822@hstat.org>
2020-04-08 22:10:54 +0000

    edit

--- _site/script.js
+++
@@ -1,9 +0,0 @@
-let ownerName = "Jazmin N"
-let userName = "jazminn7822";
-
-document.querySelectorAll(".owner-name").forEach((e) => {
-    e.innerHTML = ownerName;
-});
-
-document.querySelector("#github").href = "http://github.com/" + userName;
-document.querySelector("#fork").href = "http://github.com/" + userName + "/" + userName + ".github.io";
